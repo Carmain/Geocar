e3904bf2fc16748a8578a1e2a3f987e3ed65cf19
Marty <martin.l.billard@gmail.com>
2015-03-29 20:53:26 +0200

    Create the class (ref #10)

---
+++ app/src/main/java/com/supinfo/jva/geocar/Home.java
@@ -0,0 +1,17 @@
+package com.supinfo.jva.geocar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Martin on 29/03/2015.
+ */
+public class Home extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+}
