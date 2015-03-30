36b8c003229385c1e221d17c33a1379014a5d3a9
Marty <martin.l.billard@gmail.com>
2015-03-30 17:00:23 +0200

    About : Add the view and link it to the java class (ref #7)

--- app/src/main/java/com/supinfo/jva/geocar/About.java
+++ app/src/main/java/com/supinfo/jva/geocar/About.java
@@ -1,7 +1,15 @@
 package com.supinfo.jva.geocar;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
 /**
  * Created by Martin on 30/03/2015.
  */
-public class About {
+public class About extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
 }
