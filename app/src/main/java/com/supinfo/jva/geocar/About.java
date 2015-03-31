672654bb6a4b99ed7bb14bfd03d59d463c8a9afd
Marty <martin.l.billard@gmail.com>
2015-03-31 19:50:32 +0200

    Finish the about page (fixes #7)

--- app/src/main/java/com/supinfo/jva/geocar/About.java
+++ app/src/main/java/com/supinfo/jva/geocar/About.java
@@ -1,15 +1,43 @@
 package com.supinfo.jva.geocar;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Martin on 30/03/2015.
  */
 public class About extends ActionBarActivity {
+
+    private TextView version = null;
+    private Button gitButton = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
+        version = (TextView) findViewById(R.id.version);
+        gitButton = (Button) findViewById(R.id.button_git);
+
+        Resources res = getResources();
+        String actualVersion = res.getString(R.string.version, "0.0.1");
+        version.setText(actualVersion);
+
+        gitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String urlProject = "https://github.com/Carmain/Geocar";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(urlProject));
+                startActivity(intent);
+            }
+        });
+
     }
 }
