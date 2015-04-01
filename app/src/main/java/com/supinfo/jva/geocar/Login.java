dbffe896764507ea6f5869d9133eb3e4b2a84540
Marty <martin.l.billard@gmail.com>
2015-04-01 22:22:04 +0200

    Update the login method

--- app/src/main/java/com/supinfo/jva/geocar/Login.java
+++ app/src/main/java/com/supinfo/jva/geocar/Login.java
@@ -2,11 +2,10 @@ package com.supinfo.jva.geocar;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +23,8 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.supinfo.jva.external_class.APIRequest;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +35,8 @@ public class Login extends ActionBarActivity {
     private EditText usernameField = null;
     private EditText passwordField = null;
     private Button sendDataButton = null;
+
+    private APIRequest requestStuff = new APIRequest();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +57,16 @@ public class Login extends ActionBarActivity {
                     Toast.makeText(that, R.string.error_toast, Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    if (connectUser(username, password)) {
+                    String response = requestStuff.requestAPI(that, "login", username, password);
+                    Boolean success = false;
+                    try {
+                        JSONObject json = new JSONObject(response);
+                        success = (Boolean) json.get("success");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (success) {
                         Intent home = new Intent(Login.this, Home.class);
 
                         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(that);
@@ -71,35 +83,5 @@ public class Login extends ActionBarActivity {
                 }
             }
         });
-    }
-
-    public Boolean connectUser(String username, String password) {
-        HttpClient httpclient = new DefaultHttpClient(); // Create a new HttpClient
-        HttpPost httppost = new HttpPost("http://91.121.105.200/SUPTracking/"); // Create a post header
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        try {
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("action", "login"));
-            nameValuePairs.add(new BasicNameValuePair("login", username));
-            nameValuePairs.add(new BasicNameValuePair("password", password));
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-            // Execute HTTP Post Request
-            HttpResponse response = httpclient.execute(httppost);
-            String json_s = EntityUtils.toString(response.getEntity());
-            JSONObject json = new JSONObject(json_s);
-            return (Boolean) json.get("success");
-
-        } catch (ClientProtocolException e) {
-            Toast.makeText(this, R.string.error_connection, Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            Toast.makeText(this, R.string.error_connection, Toast.LENGTH_SHORT).show();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return false;
     }
 }
