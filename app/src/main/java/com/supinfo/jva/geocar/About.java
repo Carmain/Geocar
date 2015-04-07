54b1e3d095c54096798aadf20bc006ae4dc258de
Marty <martin.l.billard@gmail.com>
2015-04-07 20:43:39 +0200

    Use local vars instead of global

--- app/src/main/java/com/supinfo/jva/geocar/About.java
+++ app/src/main/java/com/supinfo/jva/geocar/About.java
@@ -11,16 +11,13 @@ import android.widget.TextView;
 
 public class About extends ActionBarActivity {
 
-    private TextView version = null;
-    private Button gitButton = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
 
-        version = (TextView) findViewById(R.id.version);
-        gitButton = (Button) findViewById(R.id.button_git);
+        TextView version = (TextView) findViewById(R.id.version);
+        Button gitButton = (Button) findViewById(R.id.button_git);
 
         Resources res = getResources();
         String actualVersion = res.getString(R.string.version, "1.0.0");
